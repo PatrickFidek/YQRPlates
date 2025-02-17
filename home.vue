e605f44de7dcab4436b9e6608ca15bd9ac66f71f
PatrickFidek <p-fidek@hotmail.com>
2025-02-16 18:44:25 -0600

    create laravel project

---
+++ home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <button>Log In</button>
+    </div>
+    <div>
+      <button>Pick Your Plate</button>
+    </div>
+    <div>
+      <button>Restaurants</button>
+      <button>Dashboard</button>
+    </div>
+    <div>
+      <button>Promote Now</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
