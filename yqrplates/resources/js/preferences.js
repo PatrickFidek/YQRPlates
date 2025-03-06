0dd005fd7d347925d4de7964aa0b1460a002f085
PatrickFidek <p-fidek@hotmail.com>
2025-03-06 02:11:30 -0600

    deleted old preferences.js file

--- yqrplates/resources/js/preferences.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
-       removeItemButton: true,
-       maxItemCount:5,
-       searchResultLimit:5,
-       renderChoiceLimit:5
-     }); 
-    })(jQuery);
