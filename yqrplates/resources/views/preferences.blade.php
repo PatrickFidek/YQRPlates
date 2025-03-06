e3dfa42c881ae26dcd7af7c78fd8b2b58111393a
PatrickFidek <p-fidek@hotmail.com>
2025-03-06 02:36:49 -0600

    preference fixes

--- yqrplates/resources/views/preferences.blade.php
+++ yqrplates/resources/views/preferences.blade.php
@@ -1,5 +1,5 @@
 <head>
-<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
@@ -7,13 +7,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/preferences.css') }}">
-
-<title>Preferences</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="{{ asset('js/preferences.js') }}"></script>
-
+  <title>Preferences</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="{{ asset('js/preferences.js') }}"></script>
 </head>
-
 <header>
   <nav class="p-6">
     <div class="flex justify-between items-center">
@@ -29,72 +26,69 @@
     </div>
   </nav>
 </header>
-
 <div class="jumbotron text-center">
   <h1>Enter Preferences</h1>
 </div>
-
 <div class="center">
   <div class="row">
     <div class="col-sm-2"></div>
-    <div class="col-sm-4" >
+    <div class="col-sm-4">
       <label>
         <span>Neighbourhood</span>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
         <div class="row d-flex justify-content-center mt-100">
-            <select id="choices-multiple-remove-button" multiple>
-              <option value="Neighborhood 1">Neighboorhood 1</option>
-              <option value="Neighborhood 2">Neighborhood 2</option>
-              <option value="Neighborhood 3">Neighborhood 3</option>
-            </select>
+          <select id="choices-multiple-remove-button" multiple>
+            <option value="Neighborhood 1">Neighboorhood 1</option>
+            <option value="Neighborhood 2">Neighborhood 2</option>
+            <option value="Neighborhood 3">Neighborhood 3</option>
+          </select>
         </div>
       </label>
     </div>
-    <div class="col-sm-4" >
+    <div class="col-sm-4">
       <label>
         <span>Food Type</span>
         <div class="row d-flex justify-content-center mt-100">
-            <select id="choices-multiple-remove-button" multiple>
-              <option value="Food Type">Food Type 1</option>
-              <option value="Food Type">Food Type</option>
-              <option value="Food Type">Food Type</option>
-            </select>
+          <select id="choices-multiple-remove-button" multiple>
+            <option value="Food Type">Food Type 1</option>
+            <option value="Food Type">Food Type</option>
+            <option value="Food Type">Food Type</option>
+          </select>
         </div>
-      </label>  
+      </label>
     </div>
     <div class="col-sm-2"></div>
   </div>
   <div class="row">
     <div class="col-sm-2"></div>
-    <div class="col-sm-4" >
+    <div class="col-sm-4">
       <label>
         <span>Restaurant Type</span>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
         <div class="row d-flex justify-content-center mt-100">
-            <select id="choices-multiple-remove-button" multiple>
-              <option value="Restaurant Type">Restaurant Type</option>
-              <option value="Restaurant Type">Restaurant Type</option>
-              <option value="Restaurant Type">Restaurant Type</option>
-            </select>
+          <select id="choices-multiple-remove-button" multiple>
+            <option value="Restaurant Type">Restaurant Type</option>
+            <option value="Restaurant Type">Restaurant Type</option>
+            <option value="Restaurant Type">Restaurant Type</option>
+          </select>
         </div>
       </label>
     </div>
-    <div class="col-sm-4" >
+    <div class="col-sm-4">
       <label>
         <span>Price Range</span>
         <div class="row d-flex justify-content-center mt-100">
-            <select id="choices-multiple-remove-button" multiple>
-          <option value="Price Range">Price Range</option>
-          <option value="Price Range">Price Range</option>
-          <option value="Price Range">Price Range</option>
-            </select>
+          <select id="choices-multiple-remove-button" multiple>
+            <option value="Price Range">Price Range</option>
+            <option value="Price Range">Price Range</option>
+            <option value="Price Range">Price Range</option>
+          </select>
         </div>
-      </label>  
+      </label>
     </div>
     <div class="col-sm-2"></div>
   </div>
-
   <button type="button" class="submit" background-color:#fff>Continue</button>
 </div>
