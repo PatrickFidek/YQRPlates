d1cec60274b5d0ac915325bc58a78563238fba83
PatrickFidek <p-fidek@hotmail.com>
2025-03-06 02:36:35 -0600

    dashboard fixes

--- yqrplates/resources/views/dashboard.blade.php
+++ yqrplates/resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
   <script src="{{ asset('js/dashboard.js') }}"></script>
 </head>
 <title>Dashboard</title>
-
 <header>
   <nav class="p-6">
     <div class="flex justify-between items-center">
@@ -26,59 +25,73 @@
     </div>
   </nav>
 </header>
-
 <div class="jumbotron text-center">
-  <h1>Dashboard</h1> 
+  <h1>Dashboard</h1>
 </div>
-
 <div id="portfolio" class="container-fluid text-center">
   <div class="row text-center ">
-  <div class="col-sm-6 col-xs-12">
-      <div class="panel panel-default text-center">
-        <div class="panel-heading">
-          <h1>12</h1>
-        </div>
-        <div class="panel-body">
-          <h3><strong>Type of Food</strong></h3>
-          <p><strong>Chinese, Italian</strong></p>
-        </div>
-      </div>      
-    </div> 
     <div class="col-sm-6 col-xs-12">
       <div class="panel panel-default text-center">
         <div class="panel-heading">
           <h1>12</h1>
         </div>
         <div class="panel-body">
-          <h3><strong>Type of Food</strong></h3>
-          <p><strong>Chinese, Italian</strong></p>
+          <h3>
+            <strong>Type of Food</strong>
+          </h3>
+          <p>
+            <strong>Chinese, Italian</strong>
+          </p>
         </div>
-      </div>      
-    </div> 
-    
-  </div><br>
-  <div class="row text-center ">
-  <div class="col-sm-6 col-xs-12">
-      <div class="panel panel-default text-center">
-        <div class="panel-heading">
-          <h1>12</h1>
-        </div>
-        <div class="panel-body">
-          <h3><strong>Type of Food</strong></h3>
-          <p><strong>Chinese, Italian</strong></p>
-        </div>
-      </div>      
-    </div> 
+      </div>
+    </div>
     <div class="col-sm-6 col-xs-12">
       <div class="panel panel-default text-center">
         <div class="panel-heading">
           <h1>12</h1>
         </div>
         <div class="panel-body">
-          <h3><strong>Type of Food</strong></h3>
-          <p><strong>Chinese, Italian</strong></p>
+          <h3>
+            <strong>Type of Food</strong>
+          </h3>
+          <p>
+            <strong>Chinese, Italian</strong>
+          </p>
         </div>
-      </div>      
-    </div> 
-    
+      </div>
+    </div>
   </div>
+  <br>
+  <div class="row text-center ">
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>12</h1>
+        </div>
+        <div class="panel-body">
+          <h3>
+            <strong>Type of Food</strong>
+          </h3>
+          <p>
+            <strong>Chinese, Italian</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>12</h1>
+        </div>
+        <div class="panel-body">
+          <h3>
+            <strong>Type of Food</strong>
+          </h3>
+          <p>
+            <strong>Chinese, Italian</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
