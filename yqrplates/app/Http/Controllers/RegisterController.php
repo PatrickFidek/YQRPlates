c66b3ec241d6bd6a9b6f9b5fa41d5e012bd63265
Quinn <quinnschwabe@gmail.com>
2025-03-07 13:50:07 -0600

    Included Laravel's Models class.

--- yqrplates/app/Http/Controllers/RegisterController.php
+++ yqrplates/app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class RegisterController extends Controller 
 {
