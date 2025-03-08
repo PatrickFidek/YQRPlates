a36b61ad0cb6de958684172d9f820b7da02e0e0d
PatrickFidek <p-fidek@hotmail.com>
2025-03-08 01:06:46 -0600

    preferences controller

--- yqrplates/app/Http/Controllers/PreferencesController.php
+++ yqrplates/app/Http/Controllers/PreferencesController.php
@@ -3,12 +3,8 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 
-<<<<<<< HEAD
-class PreferenceController extends Controller 
-=======
-class PreferencesController extends Controller 
->>>>>>> 0461ff733016221152f789b4fc89173f68dbfb02
-{
+
+class PreferencesController extends Controller{
     public function index() {
         return view('preferences');
     }
