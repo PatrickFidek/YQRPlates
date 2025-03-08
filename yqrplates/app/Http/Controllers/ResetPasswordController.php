df898279862f4c5e234192cbe93b189cfa6352ca
Quinn <quinnschwabe@gmail.com>
2025-03-07 23:29:14 -0600

    renamed file and controller so theyre consistent with other file names

--- yqrplates/app/Http/Controllers/ResetPasswordController.php
+++ yqrplates/app/Http/Controllers/ResetPasswordController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class PasswordResetController extends Controller 
+class ResetPasswordController extends Controller 
 {
     public function x() {
         return 0;
