118acb849d4df1c3b1ce94fc9b348708b8cf41e8
PatrickFidek <p-fidek@hotmail.com>
2025-03-08 02:39:25 -0600

    Add validation to new sign in page

--- yqrplates/resources/views/newsignin.blade.php
+++ yqrplates/resources/views/newsignin.blade.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -12,49 +8,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://yqrplates.com/css/newsignin.css">
-    <title>Sign Up</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f8f8f8;
-        }
-        .signin--container {
-            display: flex;
-            width: 800px;
-            height: 645px;
-            background: white;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            overflow: hidden;
-        }
-        .left {
-            flex: 1;
-            padding: 50px;
-            text-align: center;
-        }
-        .right {
-            width: 250px;
-            background: #ff7e2e;;
-            color: white;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            padding: 20px;
-        }
-
-        @import 'tailwindcss';
-
-
-    </style>
 </head>
+
+<title>Sign In</title>
 
 <header>
   <nav class="p-6">
@@ -71,26 +27,49 @@
     </div>
   </nav>
 </header>
+
 <body>
     <div class="signin--container">
         <div class="left">
             <h2>WELCOME TO YQR PLATES</h2>
-            <form>
+            <form action="/signin" method="POST"> @csrf 
                 <label>
-                <span>Email
-                <input type="email" required>
+                  <span>Email</span>
+                  <input name="email" type="email" value="{{ old('email')}}"/> 
+                  @if ($errors->has('email')) 
+                    <div id="email-error" class="error-message">
+                      {{$errors->first('email')}}
+                    </div> 
+                  @endif 
                 </label>
                 <label>
-                <span>Password</span>
-                <input type="password" required>
+                  <span>Password</span>
+                  <input name="password" type="password"/> 
+                  @if ($errors->has('password')) 
+                    <div id="password-error" class="error-message">
+                      {{$errors->first('password')}}
+                    </div> 
+                  @endif 
                 </label>
-                <a href="#" style="display: block; font-size: 12px; color: #999; margin-top: 5px;">Forgot password?</a>
-                <button class="submit" type="submit" style="    border: 1px solid white;">Sign In</button>
+                @if ($errors->has('message')) 
+                  <div class="error-message">
+                    {{$errors->first('message')}}
+                  </div> 
+                @endif 
+                <a href="https://www.yqrplates.com/resetpassword">
+                  <p class="forgot-pass">Forgot password?</p>
+                </a>
+                <td>
+                  <?echo "$error";?>
+                </td>
+                <button type="submit" class="submit" style="border: 1px solid white;padding-bottom: 40px;">
+                  <input type="submit" value="Sign In" style="border-bottom: none;" />
+                </button>
             </form>
         </div>
         <div class="right text-center">
-        <h3>If you already have an account, sign in <h3>
-            <button class="submit" style="width: 142px; border: 1px solid white;" onclick="location.href='https://yqrplates.com/signin';">SIGN IN</button>
+        <h3>Don't have an account? Please sign up!<h3>
+            <button class="submit" style="width: 142px; border: 1px solid white;" onclick="location.href='https://yqrplates.com/signup';">SIGN UP</button>
         </div>
     </div>
 </body>
