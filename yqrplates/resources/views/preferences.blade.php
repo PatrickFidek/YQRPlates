17acda05d2f5796c3ece1e875420b84cefc851c9
PatrickFidek <p-fidek@hotmail.com>
2025-03-09 03:26:38 -0600

    updated preferences page to support the values in database

--- yqrplates/resources/views/preferences.blade.php
+++ yqrplates/resources/views/preferences.blade.php
@@ -1,5 +1,3 @@
-
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,12 +7,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/preferences.css') }}">
-  <title>Preferences</title>
+  <title>Upload Restaurant</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="{{ asset('js/preferences.js') }}"></script>
 </head>
-
-
 <header>
   <nav class="p-6">
     <div class="flex justify-between items-center">
@@ -31,77 +27,168 @@
   </nav>
 </header>
 <div class="jumbotron text-center">
-  <h1>Enter Preferences</h1>
+<h1>Welcome, {{ auth()->user()->name }}!</h1>
 </div>
-<div class="center">
 
-  @auth
-  <h2>Welcome, {{ auth()->user()->name }}!</h2>
+    <div class="row">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-4">
+        <label>
+          <span>Neighbourhood</span>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
+          <div class="row d-flex justify-content-center mt-100">
+            <select id="choices-multiple-remove-button" multiple>
+              <option value="North West">North West</option>
+              <option value="North East">North East</option>
+              <option value="South West">South West</option>
+              <option value="South West">South East</option>
+            </select>
+          </div> @if ($errors->has('neighborhoods')) <div id="neighborhood-error" class="error-message">
+            {{$errors->first('neighborhood')}}
+          </div> @endif
+        </label>
+      </div>
+      <div class="col-sm-4">
+        <label>
+          <span>Food Type</span>
+          <div class="row d-flex justify-content-center mt-100">
+            <select id="choices-multiple-remove-button" onChange="foodType(this)">
+              <option value="" disabled></option>
+              <option value="Fast Food">Fast Food</option>
+              <option value="Canadian">Canadian</option>
+              <option value="Pizza">Pizza</option>
+              <option value="Greek">Greek</option>
+              <option value="Indian">Indian</option>
+              <option value="Sushi">Sushi</option>
+            </select>
+          </div> @if ($errors->has('food_type')) <div id="food-type-error" class="error-message">
+            {{$errors->first('food_type')}}
+          </div> @endif
+        </label>
+      </div>
+      <div class="col-sm-2"></div>
+    </div>
+    <div class="row">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-4">
+        <label>
+          <span>Restaurant Type</span>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
+          <div class="row d-flex justify-content-center mt-100">
+            <select id="choices-multiple-remove-button" multiple>
+              <option value="Dine In">Dine In</option>
+              <option value="Take Out">Take Out</option>
+              <option value="Delivery">Delivery</option>
+              <option value="Drive Thru">Drive Thru</option>
+            </select>
+          </div> @if ($errors->has('restaurant_type')) <div id="restaurant-type-error" class="error-message">
+            {{$errors->first('restaurant-type')}}
+          </div> @endif
+        </label>
+      </div>
+      <div class="col-sm-4">
+        <label>
+          <span>Price Range</span>
+          <div class="row d-flex justify-content-center mt-100">
+            <select id="choices-multiple-remove-button" onChange="priceRange(this)">
+              <option value="" disabled></option>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="Medium High">Medium High</option>
+              <option value="High">High</option>
+            </select>
+          </div> @if ($errors->has('price-range')) <div id="price-range-error" class="error-message">
+            {{$errors->first('price-range')}}
+          </div> @endif
+        </label>
+      </div>
+      <div class="col-sm-2"></div>
+    </div>
+    <button type="button" class="submit" background-color:#fff>Continue</button>
+    </form>
+    <input type="file" class="upload-input" id="imgInp" height="100%" width="100%" />
 
-  <div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-4">
-      <label>
-        <span>Neighbourhood</span>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
-        <div class="row d-flex justify-content-center mt-100">
-          <select id="choices-multiple-remove-button" multiple>
-            <option value="Neighborhood 1">Neighboorhood 1</option>
-            <option value="Neighborhood 2">Neighborhood 2</option>
-            <option value="Neighborhood 3">Neighborhood 3</option>
-          </select>
-        </div>
-      </label>
-    </div>
-    <div class="col-sm-4">
-      <label>
-        <span>Food Type</span>
-        <div class="row d-flex justify-content-center mt-100">
-          <select id="choices-multiple-remove-button" multiple>
-            <option value="Food Type">Food Type 1</option>
-            <option value="Food Type">Food Type</option>
-            <option value="Food Type">Food Type</option>
-          </select>
-        </div>
-      </label>
-    </div>
-    <div class="col-sm-2"></div>
-  </div>
-  <div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-4">
-      <label>
-        <span>Restaurant Type</span>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
-        <div class="row d-flex justify-content-center mt-100">
-          <select id="choices-multiple-remove-button" multiple>
-            <option value="Restaurant Type">Restaurant Type</option>
-            <option value="Restaurant Type">Restaurant Type</option>
-            <option value="Restaurant Type">Restaurant Type</option>
-          </select>
-        </div>
-      </label>
-    </div>
-    <div class="col-sm-4">
-      <label>
-        <span>Price Range</span>
-        <div class="row d-flex justify-content-center mt-100">
-          <select id="choices-multiple-remove-button" multiple>
-            <option value="Price Range">Price Range</option>
-            <option value="Price Range">Price Range</option>
-            <option value="Price Range">Price Range</option>
-          </select>
-        </div>
-      </label>
-    </div>
-    <div class="col-sm-2"></div>
-  </div>
-  <button type="submit" class="submit" background-color:#fff>Continue</button>
+      </div> 
 
-  @else
-  <p>You need to be logged in the view your preferences.</p>
-  @endauth
+
 
 </div>
+<script>
+  function getExtension(filename) {
+    var parts = filename.split('.');
+    return parts[parts.length - 1];
+  }
+
+  function isImage(filename) {
+    var ext = getExtension(filename);
+    switch (ext.toLowerCase()) {
+      case 'jpg':
+      case 'gif':
+      case 'bmp':
+      case 'png':
+        //etc
+        return true;
+    }
+    return false;
+  }
+
+  function isPDF(filename) {
+    var ext = getExtension(filename);
+    switch (ext.toLowerCase()) {
+      case 'pdf':
+        return true;
+    }
+    return false;
+  }
+  imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    console.log(file)
+    if (isPDF(file.name)) {
+      pdf.src = URL.createObjectURL(file)
+      prompt.style.display = "none"
+      pdf.style.display = "";
+      image.style.display = "none"
+      displayImage.style.display = "none"
+    }
+    if (isImage(file.name)) {
+      image.src = URL.createObjectURL(file)
+      prompt.style.display = "none"
+      pdf.style.display = "none";
+      image.style.display = "";
+      displayImage.style.display = "none"
+    }
+  }
+  $(document).ready(function() {
+    var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
+      removeItemButton: true,
+      maxItemCount: 15,
+      searchResultLimit: 15,
+      renderChoiceLimit: 15,
+    });
+  })(jQuery);
+
+  $(document).ready(function() {
+    $('restaurant-form').submit(function(e) {
+      e.preventDefault();
+
+      $.ajax({
+        url: $(this).attr('action'),
+        type: $(this).attr('method'),
+        data: $(this).serialize(),
+        success: function(response) {
+          window.location.href = '/profile';
+        },
+        error: function(xhr) {
+          var errors = xhr.responseJSON.errors;
+          $('.error-message').empty();
+
+          $.each(errors, function(key, value) {
+            $('#' + key + '-error').text(value[0]);
+          });
+        }
+      });
+    });
+  });
+</script>
