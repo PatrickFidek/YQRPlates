8a35b5004f0dfdd636dc52d2bbb2ce51cdd78b36
PatrickFidek <p-fidek@hotmail.com>
2025-03-10 05:03:08 -0600

    updated restaurant controller now can upload restaurants menus still WIP

--- yqrplates/app/Http/Controllers/UploadRestaurant.php
+++ yqrplates/app/Http/Controllers/UploadRestaurant.php
@@ -17,23 +17,48 @@ class UploadRestaurant extends Controller
     }
 
     public function store(Request $request){
-        $input = $request->body;;
-        $south_east = in_array("South East", $input->neighborhood);
-        $south_west = in_array("South West", $input->neighborhood);
-        $north_east = in_array("North East", $input->neighborhood);
-        $north_west = in_array("North West", $input->neighborhood);
-
-        $dine_in = in_array("Dine In", $input->restaurant_type);
-        $take_out = in_array("Take Out", $input->restaurant_type);
-        $delivery = in_array("Delivery", $input->restaurant_type);
-        $drive_thru = in_array("Drive Thru", $input->restaurant_type);
         
-        return Restaurant::create([
-            'user_id' => $input['user_id'],
-            'name' => $input['name'],
-            'menu_link' => $input['menu_link'],
-            'price_range' => $input['price_range'],
-            'food_type' => $input['food_type'],
+
+
+        $request->validate([ 
+            'restaurant_name' => ['required', Rule::unique('restaurants', 'name')],
+            'user_id' => ['required', Rule::unique('restaurants', 'user_id')],
+            'menulink' => 'nullable|url|required_without:menuimage',
+            'menuimage' => 'nullable|file|mimes:jpeg,png,pdf|max:2048|required_without:menulink',
+            'neighborhood' => 'required',
+            'restaurant_type' => 'required',
+            'price_range' => ['required', Rule::in(['Low', 'Medium', 'Medium High', 'High'])],
+            'food_type' => ['required', Rule::in(['Fast Food', 'Canadian', 'Pizza', 'Greek', 'Indian', 'Sushi', 'Italian', 'Asian', 'Chinese'])],
+        ]);
+
+        $menu_link = '';
+        $south_east = in_array("South East", $request->input('neighborhood'));
+        $south_west = in_array("South West", $request->input('neighborhood'));
+        $north_east = in_array("North East", $request->input('neighborhood'));
+        $north_west = in_array("North West", $request->input('neighborhood'));
+
+        $dine_in = in_array("Dine In", $request->input('restaurant_type'));
+        $take_out = in_array("Take Out", $request->input('restaurant_type'));
+        $delivery = in_array("Delivery", $request->input('restaurant_type'));
+        $drive_thru = in_array("Drive Thru", $request->input('restaurant_type'));
+
+        if ($request->input('menuimage') != NULL ){
+            $menu_link = $request->file('menuimage')->store('menus', 'public');
+        }
+        if($request->input('menulink') != NULL) {
+            $menu_link = $request->input('menulink');
+        }
+
+        dd($menu_link);
+ 
+
+
+        Restaurant::create([
+            'user_id' => (int)$request->input('user_id'),
+            'name' => $request->input('restaurant_name'),
+            'menu_link' => $menu_link,
+            'price_range' => $request->input('price_range'),
+            'food_type' => $request->input('food_type'),
             'south_east' => $south_east,
             'south_west' => $south_west,
             'north_east' => $north_east,
@@ -43,6 +68,8 @@ class UploadRestaurant extends Controller
             'delivery' => $delivery,
             'drive_thru' => $drive_thru
         ]);
+
+        return redirect('profile');
     }
 
     public function update(Request $request) {
