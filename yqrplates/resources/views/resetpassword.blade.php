bed251328f5e089756e70a8691e36f73b591f1e9
Quinn <quinnschwabe@gmail.com>
2025-03-10 16:30:27 -0600

    renamed field of confirm password to match Laravel's naming convetions

--- yqrplates/resources/views/resetpassword.blade.php
+++ yqrplates/resources/views/resetpassword.blade.php
@@ -66,8 +66,8 @@
 
             <label>
                 <span>Confirm New Password</span>
-                <input name='confirm' type="password" value="{{ old('confirm')}}"/>
-                @if ($errors->has('confirm')) 
+                <input name='password_confirmation' type="password" value="{{ old('password_confirmation')}}"/>
+                @if ($errors->has('password_confirmation')) 
                 <div id="password-error" class="error-message">
                   {{$errors->first('password')}}
                 </div> 
