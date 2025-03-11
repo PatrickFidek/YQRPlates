58666107a4b0a2de5c456fe4fcb87593cbb6879d
Quinn <quinnschwabe@gmail.com>
2025-03-10 23:04:54 -0600

    creation of controller file

---
+++ yqrplates/app/Http/Controllers/PromotionsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class PromotionsController extends Controller 
+{
+    
+}
