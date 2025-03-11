77e86519858dbd197b37a225f9012445975c569d
Quinn <quinnschwabe@gmail.com>
2025-03-11 00:46:25 -0600

    add promotions column to fillable array

---
+++ yqrplates/app/Models/Promotion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promotion extends Model {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type',
+        'birthday'
+    ];
+}
