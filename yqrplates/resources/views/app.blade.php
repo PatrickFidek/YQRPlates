8777950961d25bb1db370a444ae4aaf17fd3ba8a
PatrickFidek <p-fidek@hotmail.com>
2025-03-12 03:37:48 -0600

    added button to acces profile

--- yqrplates/resources/views/app.blade.php
+++ yqrplates/resources/views/app.blade.php
@@ -6,6 +6,7 @@
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
   <script src="{{ asset('js/app.js') }}"></script>
 </head>
@@ -24,6 +25,7 @@
                 </li>
                 @endguest
                 </u>
+                <button class="btn" style="margin-top: 5px" onclick="window.location.href='/profile';"><i class="fa fa-home"></i></button>
             </div>
           </div>
         </nav>
