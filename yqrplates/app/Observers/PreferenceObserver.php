57974b52a1e6a03252f77485243c49bb43176934
PatrickFidek <p-fidek@hotmail.com>
2025-03-13 04:47:11 -0600

    work on preference observer

--- yqrplates/app/Observers/PreferenceObserver.php
+++ yqrplates/app/Observers/PreferenceObserver.php
@@ -18,28 +18,52 @@ class PreferenceObserver
         $restaurant_types = 0;
         $neighborhoods = 0;
         $price_ranges = 0;
-
+      
         foreach($restaurants as $restaurant){
-                if($restaurant->food_type == $preference->food_type)
+                $dashboard = Dashboard::where('user_id', $restaurant->user_id)->first();
+                if($dashboard){
+                if($restaurant->food_type == $preference->food_type){
                         $food_types++;
-                if($restaurant->price_range == $preference->price_range)
+                        $dashboard->update(['d_food_type' => $dashboard->d_food_type + 1]);
+                }
+                if($restaurant->price_range == $preference->price_range){
                         $price_ranges++;
-                if($restaurant->south_west && $preference->south_west)
+                        $dashboard->update(['d_price_range' => $dashboard->d_price_range + 1]);
+                }
+                if($restaurant->south_west && $preference->south_west){
                         $neighborhoods++;
-                elseif($restaurant->south_east && $preference->south_east)
+                        $dashboard->update(['d_neighborhoods' => $dashboard->d_neighborhoods + 1]);
+                }
+                elseif($restaurant->south_east && $preference->south_east){
                         $neighborhoods++;
-                elseif($restaurant->north_west && $preference->north_west)
+                        $dashboard->update(['d_neighborhoods' => $dashboard->d_neighborhoods + 1]);
+                }
+                elseif($restaurant->north_west && $preference->north_west){
                         $neighborhoods++;
-                elseif($restaurant->north_east && $preference->north_east)
+                        $dashboard->update(['d_neighborhoods' => $dashboard->d_neighborhoods + 1]);
+                }
+                elseif($restaurant->north_east && $preference->north_east){
                         $neighborhoods++;
-                if ($restaurant->dine_in && $preference->dine_in)
+                       $dashboard->update(['d_neighborhoods' => $dashboard->d_neighborhoods + 1]);
+                }
+                if ($restaurant->dine_in && $preference->dine_in){
                         $restaurant_types++;
-                elseif ($restaurant->drive_thru && $preference->drive_thru)
+                       $dashboard->update(['d_restaurant_types' => $dashboard->d_restaurant_types + 1]);
+                }
+                elseif ($restaurant->drive_thru && $preference->drive_thru){
                         $restaurant_types++;
-                elseif ($restaurant->delivery && $preference->delivery)
+                       $dashboard->update(['d_restaurant_types' => $dashboard->d_restaurant_types + 1]);
+                }
+                elseif ($restaurant->delivery && $preference->delivery){
                         $restaurant_types++;
-                elseif ($restaurant->take_out && $preference->take_out)
+                       $dashboard->update(['d_restaurant_types' => $dashboard->d_restaurant_types + 1]);
+                }
+                elseif ($restaurant->take_out && $preference->take_out){
                         $restaurant_types++;
+                       $dashboard->update(['d_restaurant_types' => $dashboard->d_restaurant_types + 1]);
+                }
+                $dashboard->save();
+        }
         }
 
         Dashboard::firstOrCreate(
@@ -51,7 +75,6 @@ class PreferenceObserver
                 'd_restaurant_types' => $restaurant_types,
                 'd_price_range' => $price_ranges
         ]);
-
     }
 
     /**
@@ -59,50 +82,145 @@ class PreferenceObserver
      */
     public function updated(Preference $preference): void
     {
-        $dashboard = Dashboard::where('user_id', $preference->user_id);
+        $oldPreference = $preference->getOriginal();
+        $userDashboard = Dashboard::where('user_id', $preference->user_id);
         $restaurants = Restaurant::all();
-
         $food_types = 0;
         $restaurant_types = 0;
         $neighborhoods = 0;
         $price_ranges = 0;
-
         foreach($restaurants as $restaurant){
-        if($restaurant->food_type == $preference->food_type)
-            $food_types++;
-        if($restaurant->price_range == $preference->price_range)
+
+        $dashboard = Dashboard::where('user_id', $restaurant->user_id)->first();
+        $current = false;
+        $increase = false;
+        $decrease = false;
+        if(($oldPreference['food_type'] == $restaurant->food_type) && ($preference->food_type == $restaurant->food_type)) {
+                $current = true;
+        }
+        elseif(($oldPreference['food_type'] == $restaurant->food_type) && ($preference->food_type != $restaurant->food_type)) {
+                $decrease = true;
+        }
+        elseif(($oldPreference['food_type'] != $restaurant->food_type) && ($preference->food_type == $restaurant->food_type)) {
+                $increase = true;
+        }
+        if($current) {
+                $food_types++;
+        }
+        elseif($increase) {
+                $dashboard->increment('d_food_type');
+                $food_types++;
+        }
+        elseif($decrease){
+                $dashboard->decrement('d_food_type');
+        }
+        $current = false;
+        $increase = false;
+        $decrease = false;
+        if(($oldPreference['price_range'] == $restaurant->price_range) && ($preference->price_range == $restaurant->price_range)) {
+                $current = true;
+                }
+                elseif(($oldPreference['price_range'] == $restaurant->price_range) && ($preference->price_range != $restaurant->price_range)) {
+                $decrease = true;
+                }
+                elseif(($oldPreference['price_range'] != $restaurant->price_range) && ($preference->price_range == $restaurant->price_range)) {
+                         $increase = true;
+                }
+        if($current && !$increase) {
                 $price_ranges++;
-        if($restaurant->south_west && $preference->south_west)
-                $neighborhoods++;
-        elseif($restaurant->south_east && $preference->south_east)
-                $neighborhoods++;
-        elseif($restaurant->north_west && $preference->north_west)
-                $neighborhoods++;
-        elseif($restaurant->north_east && $preference->north_east)
-                $neighborhoods++;
-        if ($restaurant->dine_in && $preference->dine_in)
-                $restaurant_types++;
-        elseif ($restaurant->drive_thru && $preference->drive_thru)
-                $restaurant_types++;
-        elseif ($restaurant->delivery && $preference->delivery)
-                $restaurant_types++;
-        elseif ($restaurant->take_out && $preference->take_out)
+        }
+        elseif($increase) {
+                $dashboard->increment('d_price_range');
+                $price_ranges++;
+        }
+        elseif($decrease){
+                $dashboard->decrement('d_price_range');
+        }
+        
+                if($oldPreference['south_west'] && $restaurant->south_west && $preference->south_west)
+                        $current = true;
+                elseif($oldPreference['south_west'] && $restaurant->south_west && !$preference->south_west)
+                        $decrease = true;
+                elseif(!$oldPreference['south_west'] && $restaurant->south_west && $preference->south_west)
+                        $increase = true;
+                if($oldPreference['south_east'] && $restaurant->south_east && $preference->south_east)
+                        $current = true;
+                elseif($oldPreference['south_east'] && $restaurant->south_east && !$preference->south_east)
+                        $decrease = true;
+                elseif(!$oldPreference['south_east'] && $restaurant->south_east && $preference->south_east)
+                        $increase = true;
+                if($oldPreference['north_west'] && $restaurant->north_west && $preference->north_west)
+                        $current = true;
+                elseif($oldPreference['north_west'] && $restaurant->north_west && !$preference->north_west)
+                        $decrease = true;
+                elseif(!$oldPreference['north_west'] && $restaurant->north_west && $preference->north_west)
+                        $increase = true;
+                if($oldPreference['north_east'] && $restaurant->north_east && $preference->north_east)
+                        $current = true;
+                elseif($oldPreference['north_east'] && $restaurant->north_east && !$preference->north_east)
+                        $decrease = true;
+                elseif(!$oldPreference['north_east'] && $restaurant->north_east && $preference->north_east)
+                        $increase = true;
+                
+                if($current) {
+                        $neighborhoods = $neighborhoods + 1;
+                }
+                elseif($increase) {
+                        $dashboard->increment('d_neighborhoods');
+                        $neighborhoods = $neighborhoods + 1;
+                }
+                elseif($decrease){
+                        $dashboard->decrement('d_neighborhoods');
+                }
+
+        $current = false;
+        $increase = false;
+        $decrease = false;
+
+        if($oldPreference['drive_thru'] && $restaurant->drive_thru && $preference->drive_thru)
+                $current = true;
+        elseif($oldPreference['drive_thru'] && $restaurant->drive_thru && !$preference->drive_thru)
+                $decrease = true;
+        elseif(!$oldPreference['drive_thru'] && $restaurant->drive_thru && $preference->drive_thru)
+                $increase = true;
+        if($oldPreference['take_out'] && $restaurant->take_out && $preference->take_out)
+                $current = true;
+        elseif($oldPreference['take_out'] && $restaurant->take_out && !$preference->take_out)
+                $decrease = true;
+        elseif(!$oldPreference['take_out'] && $restaurant->take_out && $preference->take_out)
+                $increase = true;
+        if($oldPreference['dine_in'] && $restaurant->dine_in && $preference->dine_in)
+                $current = true;
+        elseif($oldPreference['dine_in'] && $restaurant->dine_in && !$preference->dine_in)
+                $decrease = true;
+        elseif(!$oldPreference['dine_in'] && $restaurant->dine_in && $preference->dine_in)
+                $increase = true;
+        if($oldPreference['delivery'] && $restaurant->delivery && $preference->delivery)
+                $current = true;
+        elseif($oldPreference['delivery'] && $restaurant->delivery && !$preference->delivery)
+                $decrease = true;
+        elseif(!$oldPreference['delivery'] && $restaurant->delivery && $preference->delivery)
+                $increase = true;
+        
+        if($current and !$increase) {
                 $restaurant_types++;
         }
+        elseif($increase) {
+                $dashboard->increment('d_restaurant_types');
+                $restaurant_types++;
+        }
+        elseif($decrease){
+                $dashboard->decrement('d_restaurant_types');
+        }
+                
+        }
 
-
-        $dashboard->update([
+        $userDashboard->update([
                 'd_food_type' => $food_types,
                 'd_neighborhoods' => $neighborhoods,
                 'd_restaurant_types' => $restaurant_types,
                 'd_price_range' => $price_ranges
         ]);
-        // $preference->d_food_type = $food_types;
-        // $preference->d_price_range = $price_ranges;
-        // $preference->d_restaurant_types = $restaurant_types;
-        // $preference->d_neighborhoods = $neighborhoods;
-        
-        // $preference->save();
     }
 
     /**
