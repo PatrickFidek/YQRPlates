845f254812ede3eebd0e03d6b888158ac0267810
PatrickFidek <p-fidek@hotmail.com>
2025-03-14 04:20:56 -0600

    fixed auth login error

--- yqrplates/resources/views/promotions.blade.php
+++ yqrplates/resources/views/promotions.blade.php
@@ -34,10 +34,23 @@
 
 <title>Promotions</title>
  <div class="jumbotron text-center">
+  @guest
+  <p style="color: white">You need to be logged in order to add a promotion</p>
+  <button class="btn btn-lg largebtn" onclick="window.location.href='/signin'">Sign in</button>
+  @endguest
 @auth
+<?php
+if(auth()->user()->type == "restaurant owner")
+$promotions = auth()->user()->restaurant->promotions;
+
+?>
+  @if(auth()->user()->type == "restaurant owner")
   <h1>{{ auth()->user()->restaurant->name }}</h1>
+  @elseif(auth()->user()->type == "customer")
+  <h2 style="color: white;">Sorry, only restaurant owners can add promotions</h2>
+  @endif
  </div>
-<!--@if(auth()->user()->type == "restaurant owner")-->
+ @if(auth()->user()->type == "restaurant owner")
  <div id="about" class="container-fluid" style="width: 400px">
   <div class="row">
     <div class="panel panel-default text-center">
@@ -65,17 +78,10 @@
    </div>
   </div>
 <div>
-<!--@endif-->
-<!--@endauth
-@guest
-<div class="container text-center">
-  <p>Please <a href="/signin">sigin in</a> to add or manage promotions.</p>
-</div>
-@endguest
-@auth -->
   <div id="pricing" class="container-fluid">
     <h2>Current Promotions</h2>
     <div class="row">
+    @if(auth()->user()->type == "restaurant owner")
      @forelse($promotions as $promotion)
       <div class="col-sm-4 col-xs-12">
         <div class="panel panel-default text-center">
@@ -99,10 +105,10 @@
           <p>No promotions added yet.</p>
         </div>
       @endforelse
+      @endif
     </div>
  @else
-  <p>You need to be logged in the view your promotions.</p>
-  <button class="btn btn-lg largebtn" onclick="window.location.href='/signin'">Sign in</button>
+ @endif
 @endauth  
 </div>
  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -112,8 +118,6 @@
           @csrf
           @method('DELETE')
           <input type="hidden" name="promotion_id" id="deletePromotionId"> 
-        <!--  <input type="hidden" name="restaurant_id" value="{{ auth()->user()->restaurant->id }}"> -->
-
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="width: 10px">
               <span aria-hidden="true">&times;</span>
@@ -136,102 +140,6 @@
     </div>
   </div>
 </div>
-<!--
-<div>
-  <div id="pricing" class="container-fluid">
-    <h2>Current Promotions</h2>
-    <div class="row">
-      <div class="col-sm-4 col-xs-12">
-        <div class="panel panel-default text-center">
-          <div class="panel-heading">
-            <h1>Promo 1</h1>
-          </div>
-          <div class="panel-body">
-            <p>
-              <strong>Food was amazing!!</strong>
-            <p>
-          </div>
-          <div class="panel-footer">
-            <button type="button" class="submit" data-toggle="modal" data-target="#exampleModalCenter" style="width: 100%"> Remove Promotion </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4 col-xs-12">
-        <div class="panel panel-default text-center">
-          <div class="panel-heading">
-            <h1>Promo 1</h1>
-          </div>
-          <div class="panel-body">
-            <p>
-              <strong>Food was amazing!!</strong>
-            <p>
-          </div>
-          <div class="panel-footer">
-            <button type="button" class="submit" data-toggle="modal" data-target="#exampleModalCenter" style="width: 100%"> Remove Promotion </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4 col-xs-12">
-        <div class="panel panel-default text-center">
-          <div class="panel-heading">
-            <h1>Promo 1</h1>
-          </div>
-          <div class="panel-body">
-            <p>
-              <strong>Food was amazing!!</strong>
-            <p>
-          </div>
-          <div class="panel-footer">
-            <button type="button" class="submit" data-toggle="modal" data-target="#exampleModalCenter" style="width: 100%"> Remove Promotion </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4 col-xs-12">
-        <div class="panel panel-default text-center">
-          <div class="panel-heading">
-            <h1>Promo 1</h1>
-          </div>
-          <div class="panel-body">
-            <p>
-              <strong>Food was amazing!!</strong>
-            <p>
-          </div>
-          <div class="panel-footer">
-            <button type="button" class="submit" data-toggle="modal" data-target="#exampleModalCenter" style="width: 100%"> Remove Promotion </button>
-          </div>
-        </div>
-      </div>
-    </div>
-
--->
-
-<!-- 
-   Modal
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document" style="padding-top: 15%;">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="width: 10px">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <h2 class="modal-title text-center" id="exampleModalLongTitle">Are you sure you want to remove</h2>
-            <h3 class="text-center">promo 1?</h3>
-          </div>
-          <div class="modal-body text-center">
-            <p>
-              <strong>Doing so will remove this promotion from your restaurant</strong>
-            </p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="submit" data-dismiss="modal" style="width: 25%">Cancel</button>
-            <button type="button" class="submit" style="width: 40%">Confirm Removal</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
--->
   <script>
     $('#exampleModalCenter').on('show.bs.modal', function (event) {
       var button = $(event.relatedTarget);
