bddbbe2a5f837c23c0def62ad117c4b02b2958b8
PatrickFidek <p-fidek@hotmail.com>
2025-03-14 00:58:19 -0600

    fixxed suggestion controller

--- yqrplates/resources/views/suggestion.blade.php
+++ yqrplates/resources/views/suggestion.blade.php
@@ -9,8 +9,6 @@
   <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/suggestion.css') }}">
   <script src="{{ asset('js/suggestion.js') }}"></script>
 </head>
-
-
 <title>Suggestion Generator</title>
 <header>
   <nav class="p-6">
@@ -22,17 +20,17 @@
               <a class="navbar-brand" href="http://yqrplates.com">YQR PLATES</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
-              <ul class="nav navbar-nav navbar-right">
-              @guest  
+              <ul class="nav navbar-nav navbar-right"> 
+                @guest 
                 <li>
-                    <a href="signin">SIGN IN</a>
-                </li>
-              @endguest
-              @auth
+                  <a href="signin">SIGN IN</a>
+                </li> 
+                @endguest 
+                @auth
                 <li>
                   <a href="profile">PROFILE</a>
-                </li>
-              @endauth
+                </li> 
+                @endauth 
               </ul>
             </div>
           </div>
@@ -42,102 +40,40 @@
   </nav>
 </header>
 <div class="jumbotron text-center">
-
-
   <h1>Pick Your Plate</h1>
-  
-  @if(auth()->user()->type == "customer")
 </div>
-<?php
-$generated;
-$userRestaurants = [];
-$allRestaurants = [];
-foreach($restaurants as $restaurant){
-$allRestaurants[] = $restaurant;
-if(
-  (ucwords($restaurant->food_type) == ucwords(auth()->user()->preference->food_type)) &&
-  (ucwords($restaurant->price_range) == ucwords(auth()->user()->preference->price_range))
-  &&(($restaurant->south_east && auth()->user()->preference->south_east) || 
-  ($restaurant->south_west && auth()->user()->preference->south_west) ||
-  ($restaurant->north_east && auth()->user()->preference->north_east) || 
-  ($restaurant->north_west && auth()->user()->preference->north_west)) 
-  &&(($restaurant->dine_in && auth()->user()->preference->dine_in) ||
-   ($restaurant->take_out && auth()->user()->preference->take_out) ||
-   ($restaurant->delivery && auth()->user()->preference->delivery) ||
-   ($restaurant->drive_thru && auth()->user()->prefernece->drive_thru))
-  )
-  {
-  $userRestaurants[] = $restaurant;
-}
-}
-if(!empty($userRestaurants)){
-  $arrayLength = count($userRestaurants);
-  $randomIndex = rand(0, $arrayLength - 1);
-  $randomUserRestaurant = $userRestaurants[$randomIndex]->id;
-}
 
-$count = count($allRestaurants);
-$randomNumber = rand(0, $count);
-$generated = $allRestaurants[$randomNumber]->id;
-?> 
-
-<form id="testForm" method="POST" action="{{ url()->current() }}">
-  @csrf
-<input type="hidden" name="generated" id="generatedInput" value="{{ $generated }}">
-
-<div class="container-fluid">
-  <div class="text-center">
-    <button style="
-    background: none;
-    border: none;
-    padding: 0;
-    cursor: pointer;
-" onClick="updateButtonLink(event)" id="allButton">
-    <img src="{{ asset('images/Generate.png') }}" alt="Click to Generate" height="325px" id="allImage">
-    </button>
-
-</form>
-@auth
-@if(!empty($userRestaurants))  
-  </div>
-  <div style="text-align: center; padding-top: 15px">
-
-    <p style="font-size: 18px">Use Preferences</p>
-    <form action="POST" action="{{ url()->current() }}">
+<form id="testForm" method="POST" action="/getSuggestion"> 
+  @csrf 
+  @auth
+  <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+  @endauth
+  <div class="container-fluid">
+    <div class="text-center">
+      <button style="background: none;border: none;padding: 0;cursor: pointer; " type="submit" id="allButton">
+        <img src="{{ asset('images/Generate.png') }}" alt="Click to Generate" height="325px" id="allImage">
+      </button>
+  <div class="container-fluid">
+    <div class="text-center">
+</div>
+<div style="text-align: center; padding-top: 15px">
+  @guest
+  <p style="font-size: 18px">Please <a href="/signin" style="text-decoration: none; color: black">Sign In </a>To Use Preferences</p>
+  @endguest
+  @auth
+  @if(auth()->user()->type == "customer")
+  <p style="font-size: 18px">Use Preferences</p>
     <label class="switch">
-
-      <input type="checkbox" id="preferences" onchange="updateGeneratedValue()">
+      <input type="checkbox" id="preferences" name="preference">
       <span class="slider round"></span>
     </label>
-</form>
-
-  </div>
-  @else
-  <p style="font-size: 18px">You have no restaurants that match your preferences</p>
+    @if(session('error'))
+    <p>{{ session('error') }}</p>
     @endif
+  </form>
+  @endif
   @endauth
 </div>
-@endif
-@if(auth()->user()->type == "restaurant owner")
-<h1>Only Customers Can Use The Suggestion Generator</h1>
 </div>
-@endif
 </div>
-
-<script>
-  function updateGeneratedValue() {
-    const checkbox = document.getElementById('preferences');
-    const generatedInput = document.getElementById('generatedInput');
-    @if(!empty($userRestaurants))
-    if (checkbox.checked) 
-      generatedInput.value = "{{ $randomUserRestaurant }}";
-    @endif
-     else 
-      generatedInput.value = "{{ $generated }}";
-  }
-  function updateButtonLink(event) {
-    const generatedInput = document.getElementById('generatedInput').value;
-    window.location.href = '/restaurants/details/' + generatedInput;
-    event.preventDefault();
-  }
-</script>
+</div>
